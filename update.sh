d037813ff1d865d1b1b17e16746c72d9c8466212
Tiago Alves <letiagoalves@gmail.com>
2021-01-31 21:28:43 +0000

    update

--- update.sh
+++ update.sh
@@ -1,10 +1,5 @@
 git add .
 git commit -m "update"
-if [ -n "$(git status - porcelain)" ];
-then
- echo "IT IS CLEAN"
-else
- git status
+git status
  echo "Pushing data to remote server!!!"
  git push -u origin master
-fi
